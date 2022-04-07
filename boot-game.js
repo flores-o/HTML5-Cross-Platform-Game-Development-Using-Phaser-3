9946cdffec3398c1a20edbec1f38357688fde70c
Oana Florescu <oanaflores@fb.com>
2022-04-06 23:23:13 -0700

    addTile function

--- boot-game.js
+++ boot-game.js
@@ -1,10 +1,17 @@
 
+import gameOptions from './game-options.js'
+
 class bootGame extends Phaser.Scene {
     constructor() {
         super("BootGame");
     }
     preload() {
         this.load.image("emptytile", "assets/sprites/emptytile.png");
+        this.load.spritesheet("tiles", "assets/sprites/tiles.png", {
+            frameWidth: gameOptions.tileSize,
+            frameHeight: gameOptions.tileSize
+        });
+
     }
     create() {
         console.log("game is booting...");
--- play-game.js
+++ play-game.js
@@ -7,19 +7,50 @@ class playGame extends Phaser.Scene {
     }
     create() {
         console.log("you are in playGame scene...");
+        this.boardArray = [];
         for (var i = 0; i < gameOptions.boardSize.rows; i++) {
+            this.boardArray[i] = [];
             for (var j = 0; j < gameOptions.boardSize.cols; j++) {
                 var tilePosition = this.getTilePosition(i, j);
                 this.add.image(tilePosition.x, tilePosition.y, "emptytile");
+                var tile = this.add.sprite(tilePosition.x, tilePosition.y, "tiles", 3);
+                tile.visible = false;
+                this.boardArray[i][j] = {
+                    tileValue: 0,
+                    tileSprite: tile
+                }
+
             }
         }
+        this.addTile();
+        this.addTile();
     }
-    getTilePosition(row, col){
+    getTilePosition(row, col) {
         var posX = gameOptions.tileSpacing * (col + 1) + gameOptions.tileSize *
-          (col + 0.5);
+            (col + 0.5);
         var posY = gameOptions.tileSpacing * (row + 1) + gameOptions.tileSize *
-          (row + 0.5);
+            (row + 0.5);
         return new Phaser.Geom.Point(posX, posY);
+    }
+
+    addTile() {
+        var emptyTiles = [];
+        for (var i = 0; i < gameOptions.boardSize.rows; i++) {
+            for (var j = 0; j < gameOptions.boardSize.cols; j++) {
+                if (this.boardArray[i][j].tileValue == 0) {
+                    emptyTiles.push({
+                        row: i,
+                        col: j
+                    })
+                }
+            }
+        }
+        if (emptyTiles.length > 0) {
+            var chosenTile = Phaser.Utils.Array.GetRandom(emptyTiles);
+            this.boardArray[chosenTile.row][chosenTile.col].tileValue = 1;
+            this.boardArray[chosenTile.row][chosenTile.col].tileSprite.visible = true;
+            this.boardArray[chosenTile.row][chosenTile.col].tileSprite.setFrame(0);
+        }
     }
 }
 export default playGame
