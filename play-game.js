f78b8effe4e47a06c94930c7cffd1292285c062d
Oana Florescu <oanaflores@fb.com>
2022-04-07 10:45:14 -0700

    listens for drag and drop/swipe event

--- play-game.js
+++ play-game.js
@@ -28,7 +28,7 @@ class playGame extends Phaser.Scene {
 
         // waiting for player input
         this.input.keyboard.on("keydown", this.handleKey, this);
-        //this.input.on("pointerup", this.handleSwipe, this);
+        this.input.on("pointerup", this.handleSwipe, this);
     }
     getTilePosition(row, col) {
         var posX = gameOptions.tileSpacing * (col + 1) + gameOptions.tileSize *
@@ -71,6 +71,14 @@ class playGame extends Phaser.Scene {
     handleKey(e){
         var keyPressed = e.code
         console.log("You pressed key #" + keyPressed);
+    }
+
+    handleSwipe(e){
+        var swipeTime = e.upTime - e.downTime;
+        var swipe = new Phaser.Geom.Point(e.upX - e.downX, e.upY - e.downY);
+        console.log("Movement time:" + swipeTime + " ms");
+        console.log("Horizontal distance: " + swipe.x + " pixels");
+        console.log("Vertical distance: " + swipe.y + " pixels");
     }
 }
 export default playGame
