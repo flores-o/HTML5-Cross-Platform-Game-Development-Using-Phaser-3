0764713c20f62d18a02250d057f6302b96192547
Oana Florescu <oanaflores@fb.com>
2022-04-12 23:13:26 -0700

    cleaning up code from previous commit

--- play-game.js
+++ play-game.js
@@ -75,12 +75,10 @@ class playGame extends Phaser.Scene {
         var dRow = (d == LEFT || d == RIGHT) ? 0 : d == UP ? -1 : 1;
         var dCol = (d == UP || d == DOWN) ? 0 : d == LEFT ? -1 : 1;
         this.canMove = false;
-        // var movedTiles = 0;
         var firstRow = (d == UP) ? 1 : 0;
         var lastRow = gameOptions.boardSize.rows - ((d == DOWN) ? 1 : 0);
         var firstCol = (d == LEFT) ? 1 : 0;
         var lastCol = gameOptions.boardSize.cols - ((d == RIGHT) ? 1 : 0);
-        // var movedSomething = false;
         for (var i = firstRow; i < lastRow; i++) {
             for (var j = firstCol; j < lastCol; j++) {
                 var curRow = dRow == 1 ? (lastRow - 1) - i : i;
@@ -93,14 +91,8 @@ class playGame extends Phaser.Scene {
                         newRow += dRow;
                         newCol += dCol;
                     }
-                    // movedTiles++;
                     if (newRow != curRow || newCol != curCol) {
-                        // movedSomething = true;
-
-                        // this.boardArray[curRow][curCol].tileSprite.depth = movedTiles;
                         var newPos = this.getTilePosition(curRow + dRow, curCol + dCol);
-                        // this.boardArray[curRow][curCol].tileSprite.x = newPos.x;
-                        // this.boardArray[curRow][curCol].tileSprite.y = newPos.y;
                         this.moveTile(this.boardArray[curRow][curCol].tileSprite, newPos);
                         this.boardArray[curRow][curCol].tileValue = 0;
                         if (this.boardArray[newRow][newCol].tileValue == tileValue) {
@@ -115,11 +107,6 @@ class playGame extends Phaser.Scene {
                 }
             }
         }
-        // if (movedSomething) {
-        //    this.refreshBoard();
-        // } else {
-           // this.canMove = true;
-        // }
     }
 
     moveTile(tile, point){
